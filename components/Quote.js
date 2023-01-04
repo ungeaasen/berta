9c556f1638a28ea56cea15818ff4cd9c0132bbb5
aasenaasen <aasenoscar@gmail.com>
2023-01-04 14:45:41 +0100

    updated quote component now working

--- components/Quote.js
+++ components/Quote.js
@@ -4,7 +4,7 @@ const Quote = () => {
     return(
         <div className="quote">
             <div className="quoteContainer">
-                <p>"You can quote me on that!"</p>
+                <p>`"You can quote me on that!"`</p>
                 <p> -Jens Arildsen</p>
             </div>
         </div>
