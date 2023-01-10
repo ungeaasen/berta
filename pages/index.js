a2ce37d54c93243c0254bf60fc303bbb2fa7d119
aasenaasen <aasenoscar@gmail.com>
2023-01-10 12:14:59 +0100

    google analytics implement v2

--- pages/index.js
+++ pages/index.js
@@ -14,23 +14,23 @@ export default function Home({blogs}) {
     <PageLayout>
       <Head>
         <title>Berta internkommunikasjon</title>
-        <Script 
+        <meta property="og:title" content="Berta internkommunikasjon. Det er insiden som teller." key="title" />
+      </Head>
+      <Script 
           strategy="lazyOnload"
           src={`https://www.googletagmanager.com/gtag/js?id=G-SEPDQV6SSGid=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         />
           
         <Script strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag(‘js’, new Date());
-          gtag(‘config’, ‘${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}’, {
-          page_path: window.location.pathname,
-          });
-        `}
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag(‘js’, new Date());
+            gtag(‘config’, ‘${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}’, {
+            page_path: window.location.pathname,
+            });
+          `}
         </Script>
-        <meta property="og:title" content="Berta internkommunikasjon. Det er insiden som teller." key="title" />
-      </Head>
       <Navbar expand="lg">
         <div className="tittel">
           <Navbar.Brand href="#home"><p>BERTA</p></Navbar.Brand>
