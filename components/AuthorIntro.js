8cf6cfb130e14a63e8d36b099502b7d2fb457673
aasenaasen <aasenoscar@gmail.com>
2023-01-11 13:25:27 +0100

    styling

--- components/AuthorIntro.js
+++ components/AuthorIntro.js
@@ -6,7 +6,8 @@ const AuthorIntro = () =>
         <h1>Gode beslutninger kommer av god intern-kommunikasjon</h1>
     </div>
     <div className='introTekst'>
-      <p> Berta gjør virksomheter mer bevisst på hvordan ansatte kommuniserer innad. Vi gjør undersøkelser og identifiserer hvordan internkommunikasjonen deres fungerer – og levere en rapport med alle nøkkelfunn.</p>
+      <p> Berta gjør virksomheter mer bevisst på hvordan ansatte kommuniserer innad. 
+        Vi gjør undersøkelser og identifiserer hvordan internkommunikasjonen deres fungerer – og levere en rapport med alle nøkkelfunn.</p>
     </div>
   </div>
 
