794fcc06dc0f2c242ea2df2083fd1b1614bf92de
ungeaasen <ungeaasen@gmail.com>
2023-02-17 10:38:20 +0100

    styling

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -1,7 +1,4 @@
 
-//import { Container } from "react-bootstrap";
-//import Row from 'react-bootstrap/Row';
-
 export default function PageLayout({children}) {
   return (
     <div className="container">
