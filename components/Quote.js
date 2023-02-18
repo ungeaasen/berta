a8d799e6962d4c51699e2132be87062d14336318
ungeaasen <ungeaasen@gmail.com>
2023-02-18 12:59:47 +0100

    styling for mobile

--- components/Quote.js
+++ components/Quote.js
@@ -2,11 +2,12 @@ const Quote = () => {
     return(
         <div className="quote">
             <div className="quoteContainer">
-            <span className="quoteSign">&quot;I</span>
-                <p>nternkommunikasjon er noe mange virksomheter undervurderer. 
+                <p>
+                    <span className="quoteSign">&quot;</span>Internkommunikasjon er noe mange virksomheter undervurderer. 
                     Det å kommunisere like godt internt som eksternt, er et viktig virkemiddel for å lykkes.
+                    <span className="quoteSign">&quot;</span>
                 </p>
-            <span className="quoteSign">&quot;</span>
+           
             </div>
             <div className="quoter">
                 <p>– Line Kristine Moen Larsen, Leveranseleder kultur og struktur NAF</p>
