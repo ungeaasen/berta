cf4f214bd3b36c8a691f0e95306881de38433b65
ungeaasen <ungeaasen@gmail.com>
2023-02-22 19:50:47 +0100

    footer styling fix

--- components/Footer.js
+++ components/Footer.js
@@ -6,12 +6,12 @@ const Footer = () => {
             <div className="pageFooterContainer">
            <div className="contact">
             <a href="mailto:hei@berta.no">Epost</a>{' | '}
-            <span>Tlf: 411 05 497 </span>
+            <a href="https://www.linkedin.com/company/ertaberta/">Linkedin</a>{' | '}
+            <a href="tel:Tlf: 411 05 497">Telefon</a>
             {' | '}
+            <a href="#">Berta 2023</a>
             </div>
-            <div className="logo">
-           <span> berta 2023</span>
-            </div>
+
             </div>
         </footer>
     )
