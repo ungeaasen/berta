1b486ca1690c79e615ef12839f80dc962311b01f
aasenaasen <aasenoscar@gmail.com>
2023-03-07 07:45:16 +0100

    footWerk

--- components/Footer.js
+++ components/Footer.js
@@ -4,14 +4,13 @@ const Footer = () => {
     return (
         <footer className="pageFooter">
             <div className="pageFooterContainer">
-           <div className="contact">
-            <a href="mailto:hei@berta.no">Epost</a>{' | '}
-            <a href="https://www.linkedin.com/company/ertaberta/">Linkedin</a>{' | '}
-            <a href="tel:Tlf: 411 05 497">Telefon</a>
-            {' | '}
-            <a href="#">Berta 2023</a>
-            </div>
-
+                <div className="contact">
+                    <a href="mailto:hei@berta.no">Epost</a>{' | '}
+                    <a href="https://www.linkedin.com/company/ertaberta/">Linkedin</a>{' | '}
+                    <a href="tel:Tlf: 411 05 497">Telefon</a>
+                    {' | '}
+                    <a href="#">Berta 2023</a>
+                </div>
             </div>
         </footer>
     )
